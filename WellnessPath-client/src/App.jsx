b3ebebde9d7ecbe2a9f1b272d5b2a4b9e707ee43
Geetha R Kumar <geetha2work@gmail.com>
2024-10-15 19:56:27 -0400

    Initial Folder structure and install dependencies

---
+++ WellnessPath-client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='text-green-500'>Welcome to Wellness path!</h1>
+   
+    </>
+  )
+}
+
+export default App
