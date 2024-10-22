5742db9ed7864f9217d2ccdd012bb003439e1717
Geetha R Kumar <geetha2work@gmail.com>
2024-10-22 19:28:25 -0400

    Add appointment slots

--- WellnessPath-client/src/pages/Appointment/Appointment.jsx
+++ WellnessPath-client/src/pages/Appointment/Appointment.jsx
@@ -13,10 +13,12 @@ const Appointment = () => {
   const [slotTime,setSlotTime] = useState('');
 
   useEffect(()=>{
+    console.log(docId,"docId")
     if(docId){
       getDoctorById(docId);
+      console.log("selectedDoctor",selectedDoctor)
     }
-  },[docId]);
+  },[docId,getDoctorById]);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error}</p>;
@@ -25,15 +27,48 @@ const Appointment = () => {
     setDocSlots([]);
     //getting ucrrent date
     let today = new Date()
-    for(let i = 0;i<7;i++){
+    for(let i = 0;i < 14;i++){
+      //getting date with index
       let currentDate = new Date(today);
-      currentDate.set
+      currentDate.setDate(today.getDate()+i)
+
+      //setting end time of the date using index
+      let endTime = new Date()
+      endTime.setDate(today.getDate()+1);
+      endTime.setHours(21,0,0,0)
+
+      //setting hours
+      if(today.getDate() === currentDate.getDate()){
+        currentDate.setHours(currentDate.getHours() > 10 ? currentDate.getHours() + 1 : 10 )
+        currentDate.setMinutes(currentDate.getMinutes() > 30 ? 30 : 0)
+
+      }else {
+        currentDate.setHours(10)
+        currentDate.setMinutes(10)
+      }
+
+      let timeSlots = [];
+      while (currentDate < endTime){
+        let formattedTime = currentDate.toLocaleTimeString([],{ hour: '2-digit',minute: '2-digit'})
+        //add slot to array
+        timeSlots.push({
+          datetime: new Date(currentDate),
+          time:formattedTime
+        })
+        //increment current time by 30 minutes
+        currentDate.setMinutes(currentDate.getMinutes()+30)
+      }
+      setDocSlots(prev => ([...prev,timeSlots]))
     }
   }
   useEffect(()=>{
     getAvailableSlots();
-  },[docInfo]);
+  },[selectedDoctor]);
 
+  useEffect(()=>{
+    console.log(docSlots);
+  },[docSlots])
+  console.log(selectedDoctor);
   return (
     <>
       <div className='bg-image3'></div>
